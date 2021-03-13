bbfb6a945710649e11e48327b7205ea970bcae80
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-03-13 15:42:37 +0700

    Assignment: Data Types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,6 +1,13 @@
 let country = 'Russia';
 let continent = 'Eurasia';
 let population = 144;
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
