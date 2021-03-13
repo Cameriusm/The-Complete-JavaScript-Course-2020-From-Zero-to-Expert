99de10421265eff468e332f81c7b87b9fa4478b9
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-03-13 15:57:11 +0700

    Assignment: Let, const and var

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,13 +1,17 @@
-let country = 'Russia';
-let continent = 'Eurasia';
+const country = 'Russia';
+const continent = 'Eurasia';
 let population = 144;
 // console.log(country);
 // console.log(continent);
 // console.log(population);
 
-let isIsland = false;
+const isIsland = false;
 let language;
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+
+language = 'Russian';
+
+isIsland = true;
