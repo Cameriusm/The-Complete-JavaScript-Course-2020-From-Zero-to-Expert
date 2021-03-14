43afbfcf3881afc8234cf69f0fbf10107a523c18
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-03-14 16:46:30 +0700

    Assignment: Type Conversion and Coercion

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -33,10 +33,16 @@ language = 'Russian';
 // const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
 // console.log(description);
 
-if (population > 33) {
-  console.log(`${country}'s population is above average.`);
-} else {
-  console.log(
-    `${country}'s population is ${33 - population} million below average`
-  );
-}
+// if (population > 33) {
+//   console.log(`${country}'s population is above average.`);
+// } else {
+//   console.log(
+//     `${country}'s population is ${33 - population} million below average`
+//   );
+// }
+
+console.log('9' - '5'); // 4
+console.log('19' - '13' + '17'); // 617
+console.log('19' - '13' + 17); // 23
+console.log('123' < 57); // false
+console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
