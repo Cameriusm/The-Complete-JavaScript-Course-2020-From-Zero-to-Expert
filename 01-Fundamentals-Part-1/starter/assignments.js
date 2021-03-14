9c8b46d60178fb1d7433b17f71111362ac2ac0b6
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-03-14 14:25:56 +0700

    Assignment: Strings and Template Literals

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -16,16 +16,19 @@ language = 'Russian';
 
 // isIsland = true;
 
-console.log(population / 2);
-population++;
-console.log(population);
-console.log(population > 6);
-console.log(population < 33);
-const description =
-  country +
-  'is in' +
-  continent +
-  ', and its ' +
-  population +
-  'million people speak' +
-  language;
+// console.log(population / 2);
+// population++;
+// console.log(population);
+// console.log(population > 6);
+// console.log(population < 33);
+// const description =
+//   country +
+//   ' is in ' +
+//   continent +
+//   ', and its ' +
+//   population +
+//   ' million people speak ' +
+//   language;
+// console.log(description);
+const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+console.log(description);
