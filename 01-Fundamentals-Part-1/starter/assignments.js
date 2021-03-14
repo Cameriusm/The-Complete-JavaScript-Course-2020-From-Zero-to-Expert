06fb7f33f8fe70c94ae731409092633d1192e4d1
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-03-14 15:54:16 +0700

    Assignment: Taking Decisions if else Statements

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -30,5 +30,13 @@ language = 'Russian';
 //   ' million people speak ' +
 //   language;
 // console.log(description);
-const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
-console.log(description);
+// const description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+// console.log(description);
+
+if (population > 33) {
+  console.log(`${country}'s population is above average.`);
+} else {
+  console.log(
+    `${country}'s population is ${33 - population} million below average`
+  );
+}
