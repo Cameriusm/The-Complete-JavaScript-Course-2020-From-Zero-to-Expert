d516729767ed7cb0c37d3a692c91dfd4329b1561
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-03-29 22:20:41 +0700

    Assignment: Function Declarations vs. Expressions

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function describeCountry(country, population, capitalCity) {
   return `${country} has ${population} million people and its capital city is ${capitalCity}`;
 }
@@ -10,3 +10,22 @@ const descGermany = describeCountry('Germany', 83, 'Berlin');
 console.log(descRussia);
 console.log(descFinland);
 console.log(descGermany);
+*/
+
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percentageRussia1 = percentageOfWorld1(144);
+const percentageFinland1 = percentageOfWorld1(6);
+const percentageGermany1 = percentageOfWorld1(83);
+console.log(percentageRussia1, percentageFinland1, percentageGermany1);
+
+const percentageOfWorld2 = function (population) {
+  return (population / 7900) * 100;
+};
+
+const percentageRussia2 = percentageOfWorld1(144);
+const percentageFinland2 = percentageOfWorld1(6);
+const percentageGermany2 = percentageOfWorld1(83);
+console.log(percentageRussia2, percentageFinland2, percentageGermany2);
