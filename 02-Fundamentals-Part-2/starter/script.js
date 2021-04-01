e0ae0ab0ac7143a5f0930bd65ff8d92a075a43ef
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-04-01 21:43:33 +0700

    Dot vs. Bracket Notation

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -271,7 +271,7 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
 
 console.log(total, bills, tips);
-*/
+
 
 const jonasArray = [
   'jonas',
@@ -288,3 +288,47 @@ const jonas = {
   job: 'teacher',
   friends: ['Michael', 'Peter', 'Steven'],
 };
+*/
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+};
+
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+// console.log(jonas.'last' + nameKey)
+
+const interestedIn = prompt(
+  'What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends'
+);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log(
+    console.log(
+      'Wrong request! Choose between firstName, lastName, age, job, and friends'
+    )
+  );
+}
+
+jonas.location = 'Portugal';
+jonas['lwitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+// Challenge
+// "Jonas has 3 friends, and his best friend is called Michael"
+
+console.log(
+  `Jonas has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+);
