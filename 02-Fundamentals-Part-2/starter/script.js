a360c904edfd6e5ad16645ec8809e3a4907c0a41
Cameriusm <56196357+Cameriusm@users.noreply.github.com>
2021-06-30 14:56:44 +0700

    Looping Backwards and Loops in Loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -440,15 +440,15 @@ for (let rep = 1; rep <= 10; rep++) {
   console.log(`Lifting weights repitition ${rep}`);
 }
 */
-const jonas = [
-  'jonas',
-  'Schmedtmann',
-  2037 - 1991,
-  'teacher',
-  ['Michael', 'Peter', 'Steven'],
-  true,
-];
-const types = [];
+// const jonas = [
+//   'jonas',
+//   'Schmedtmann',
+//   2037 - 1991,
+//   'teacher',
+//   ['Michael', 'Peter', 'Steven'],
+//   true,
+// ];
+// const types = [];
 
 // console.log(jonas[0])
 // console.log(jonas[1])
@@ -456,33 +456,55 @@ const types = [];
 // console.log(jonas[4])
 // jonas[5] does NOT exist
 
-for (let i = 0; i < jonas.length; i++) {
-  // Reading from jonas array
-  console.log(jonas[i], typeof jonas[i]);
+// for (let i = 0; i < jonas.length; i++) {
+//   // Reading from jonas array
+//   console.log(jonas[i], typeof jonas[i]);
 
-  // Filling types of array
-  // types[i] = typeof jonas[i];
-  types.push(typeof jonas[i]);
-}
-console.log(types);
+//   // Filling types of array
+//   // types[i] = typeof jonas[i];
+//   types.push(typeof jonas[i]);
+// }
+// console.log(types);
 
-const years = [1991, 2007, 1969, 2020];
-const ages = [];
+// const years = [1991, 2007, 1969, 2020];
+// const ages = [];
 
-for (let i = 0; i < years.length; i++) {
-  ages.push(2037 - years[i]);
-}
-console.log(ages);
+// for (let i = 0; i < years.length; i++) {
+//   ages.push(2037 - years[i]);
+// }
+// console.log(ages);
 
 // continue and break
-console.log('--- ONLY STRING ---');
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] !== 'string') continue;
-  console.log(jonas[i], typeof jonas[i]);
+// console.log('--- ONLY STRING ---');
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] !== 'string') continue;
+//   console.log(jonas[i], typeof jonas[i]);
+// }
+
+// console.log('--- BREAK WITH NUMBER ---');
+// for (let i = 0; i < jonas.length; i++) {
+//   if (typeof jonas[i] === 'number') break;
+//   console.log(jonas[i], typeof jonas[i]);
+// }
+
+const jonas = [
+  'jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven'],
+];
+
+// 0, 1, ..., 4
+// 4, 3, ..., 0
+
+for(let i = jonas.length - 1; i >= 0; i--){
+  console.log(i, jonas[i])
 }
 
-console.log('--- BREAK WITH NUMBER ---');
-for (let i = 0; i < jonas.length; i++) {
-  if (typeof jonas[i] === 'number') break;
-  console.log(jonas[i], typeof jonas[i]);
+for(let exercise = 1; exercise < 4; exercise++){
+  console.log(`-------- Starting exercise ${exercise}`)
+  for(let rep = 1; rep < 6; rep++){
+    console.log(`Exercise ${exercise}: Lifting weights repetition ${rep} :D`)
+  }
 }
